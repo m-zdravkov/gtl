8a315265b57d92dc6d8f46f77b7a49f54b1bb5e2
m-zdravkov <michael.zdravkov@gmail.com>
2019-05-09 23:27:12 +0200

    added getUser for searching by ssn

--- app/communication/resources/UserResources.ts
+++ app/communication/resources/UserResources.ts
@@ -1,9 +1,13 @@
 import { defaultCtrlCall } from './UtilResources';
 import { Router } from 'express';
-import { createUser } from '../../controllers/user/UserCtrl';
+import { createUser, getUser } from '../../controllers/user/UserCtrl';
 
 export function userHandler(router: Router): void {
     router.post('/users', (req, res) => {
         return defaultCtrlCall(res, createUser, req);
+    });
+
+    router.get('/users', (req, res) => {
+        return defaultCtrlCall(res, getUser, req);
     });
 }
--- app/controllers/user/UserCtrl.ts
+++ app/controllers/user/UserCtrl.ts
@@ -3,6 +3,7 @@ import { UserService } from '../../services/user/UserService';
 import { getConnection } from '../../components/database/DbConnect';
 import { DocUser } from '../../models/user/User';
 import { ErrorHandler } from '../../components/ErrorHandler';
+import { stringify } from 'querystring';
 
 export async function createUser(req: Request): Promise<DocUser> {
   const fName = 'UserCtrl.createUser';
@@ -13,5 +14,24 @@ export async function createUser(req: Request): Promise<DocUser> {
     return userService.create(reqBody).save();
   } catch (e) {
     throw ErrorHandler.handleErrValidation(fName, e.msg, e.inner);
+  }
+}
+
+export async function getUser(req: Request): Promise<DocUser> {
+  const fName = 'UserCtrl.getUser';
+
+  const queryUser: {
+    ssn: string;
+  } = {
+    ssn: req.query.ssn
+  };
+
+  const db = await getConnection();
+  const userService = new UserService(db);
+
+  try {
+    return userService.findOne(queryUser);
+  } catch (e) {
+    throw ErrorHandler.handleErrQueryfunction(fName, e.msg, e.inner);
   }
 }
