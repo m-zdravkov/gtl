3ac5d7db503ea7d81e396213ca9774085b319dd8
Dimitar Stanchev Stefanov <dsstefanov2@gmail.com>
2019-05-12 16:48:46 +0200

    return a book functionality

--- app/controllers/user/UserCtrl.ts
+++ app/controllers/user/UserCtrl.ts
@@ -3,6 +3,8 @@ import { UserService } from '../../services/user/UserService';
 import { getConnection } from '../../components/database/DbConnect';
 import { DocUser } from '../../models/user/User';
 import { ErrorHandler } from '../../components/ErrorHandler';
+import { BookCopyService } from '../../services/book/BookCopyService';
+import { DocBookCopy } from '../../models/book/BookCopy';
 
 export async function createUser(req: Request): Promise<DocUser> {
   const fName = 'UserCtrl.createUser';
@@ -56,4 +58,25 @@ export async function updateUser(req: Request): Promise<DocUser> {
 
   await existingUser.save();
   return userService.findOne({ssn: user.ssn});
+}
+
+export async function returnBook(req: Request): Promise<void> {
+  const fName = 'UserCtrl.returnBook';
+  const db = await getConnection();
+  const userService = new UserService(db);
+  const bookCopyService = new BookCopyService(db);
+  const user = await userService.findOne({ssn: req.params.ssn}, 'takenBooks', 'takenBooks');
+  if (!user) {
+    throw ErrorHandler.handleErrDb(fName, 'User not found');
+  }
+  user.takenBooks = user.takenBooks as DocBookCopy[];
+  const copy = user.takenBooks.find(copy => {
+    copy = copy as DocBookCopy;
+    return copy._id.toHexString() === req.params.bookCopyId;
+  }) as DocBookCopy;
+  if (!copy) {
+    throw ErrorHandler.handleErrDb(fName, 'The book copy has already been returned');
+  }
+  await bookCopyService.resetCopy(copy);
+  await user.update({ $pull: { takenBooks: copy._id } })
 }
--- app/services/book/BookCopyService.ts
+++ app/services/book/BookCopyService.ts
@@ -1,9 +1,23 @@
 import { Connection } from 'mongoose';
 import { BaseService } from '../BaseService';
-import { DocBookCopy, LeanBookCopy } from '../../models/book/BookCopy';
+import { BookCopy, DocBookCopy, LeanBookCopy } from '../../models/book/BookCopy';
 
 export class BookCopyService extends BaseService<LeanBookCopy, DocBookCopy> {
   constructor(db: Connection) {
     super('BookCopy', db);
+  }
+
+  async resetCopy(copy: DocBookCopy): Promise<DocBookCopy> {
+    await copy.update({
+      $set: {
+        available: true,
+      },
+      $unset: {
+        expectedReturnDate: '',
+        reminderSent: '',
+        takenDate: ''
+      }
+    });
+    return this.findById(copy._id);
   }
 }
